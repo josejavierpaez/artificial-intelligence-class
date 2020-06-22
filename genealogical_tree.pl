62f53564492973a9a60fe7d6bbb15ba1cdbb83f7
josejavierpaez <josejavierpaez@hotmail.com>
2020-06-21 20:57:46 -0400

    add  genealogical tree

---
+++ genealogical_tree.pl
@@ -0,0 +1,198 @@
+
+% Hombres
+hombre(christia_xI).
+hombre(george_I).
+hombre(frederik_vIII).
+hombre(valdemar).
+hombre(cristian_x).
+hombre(harald).
+hombre(gustavus).
+hombre(haakon_vII).
+hombre(eduard_vII).
+hombre(alexndreIII).
+hombre(ernest_august_de_anover).
+hombre(frederik_lx).
+hombre(knud).
+hombre(frederick_schaumburh).
+hombre(charles_de_suecia).
+hombre(carl).
+hombre(jurgen_castenkiold).
+hombre(olaf_v).
+hombre(margrete_II).
+hombre(axel_de_dinamarca).
+hombre(ranhild_v).
+hombre(ingolf).
+hombre(christian).
+hombre(leopold_III).
+hombre(frederick).
+hombre(joachim).
+hombre(gustav).
+hombre(constantino_II).
+hombre(paulos).
+hombre(nikolaos).
+hombre(phillippos).
+hombre(ragnhild).
+hombre(harald_v).
+hombre(johan_martin_ferner).
+hombre(haakon_magnus).
+hombre(christian).
+hombre(vincent).
+hombre(nicolas).
+hombre(felix).
+hombre(henrik).
+hombre(alexander_johannsmaanny).
+hombre(aristides).
+hombre(ari_behn).
+hombre(sverre).
+hombre(marius).
+
+% Mujeres
+
+:- discontiguous mujer/1.
+mujer(louise_de_hesse_cassel).
+mujer(dagmar).
+mujer(alexandra).
+mujer(thyra).
+mujer(louise_de_suecia).
+mujer(dagmar).
+mujer(maud).
+mujer(holga_de_rusia).                  
+mujer(marie_orleans_chartres).
+mujer(alexandrine_mecklenburg).
+mujer(louise).
+mujer(helen_holstein).
+mujer(ingeborg).
+mujer(margaret).
+mujer(astrid).
+mujer(martha).
+mujer(ingri_de_suecia).
+mujer(anne_marie).
+mujer(carolina_matilda).
+mujer(benedikte).
+mujer(elisabeth).
+mujer(elsa_von_rosen).
+mujer(anne_larsson).
+mujer(kristine_rivelsrud).
+mujer(henrry_de_laborde_de_monpezat).
+mujer(nathalie).
+mujer(alexia).
+mujer(theodora).
+mujer(inge_terney).
+mujer(sussie_pedersen).
+mujer(erling_sven_lorentzen).
+mujer(sonia_haraldsen).
+mujer(martha_louise).
+mujer(mary_donaldson).
+mujer(isabella).
+mujer(josephine).
+mujer(atena).
+mujer(achilea).
+mujer(maria_olympia).
+mujer(adyssea).
+mujer(maud).
+muejer(leah).
+mujer(emma).
+mujer(ingrid).
+
+
+
+% Matrimonios
+
+:- discontiguous casados/2.
+casados(christian_xi,louise_de_hesse_cassel).
+casados(frederik_vIII,louise_de_suecia).
+casados(eduard_vII,alexandra).
+casados(george_I,holga_de_rusia). % no tuvieron hijos
+casados(alexndreIII,dagmar). % no tuvieron hijos
+casados(ernest_august_de_anover,thyra). % no tuvieron hijos
+casados(valdemar,marie_orleans_chartres). % no tuvieron hijos
+casados(cristian_x,alexandrine_mecklenburg).
+casados(frederick_schaumburh,louise). % no tuvieron hijos
+casados(harald,helen_holstein).
+casados(charles_de_suecia,ingeborg).
+casados(jurgen_castenkiold,dagmar). % no tuvieron hijos
+casados(olaf_v,maud). % no tuvieron hijos
+casados(frederik_lx,ingri_de_suecia).
+casados(knud,carolina_matilda).
+casados(axel_de_dinamarca,margaret). %no tuvieron hijos
+casados(leopold_III,astrid).  %no tuvieron hijos
+casados(carl,elsa_von_rosen). %no tuvieron hijos
+casados(carl,anne_larsson). %no tuvieron hijos
+casados(carl,kristine_rivelsrud). %no tuvieron hijos
+casados(olaf_v,martha).
+casados(margrete_II,henrry_de_laborde_de_monpezat).
+casados(richard_zu_saynwittgenstein,benedikte).
+casados(constantino_II,anne_marie).
+casados(ingolf,inge_terney). %no tuvieron hijos.
+casados(ingolf,sussie_pedersen). %no tuvieron hijos.
+casados(christian,anne_dorte). %no tuvieron hijos.
+casado(ragnhild,erling_sven_lorentzen). %no tuvieron hijos.
+casados(johan_martin_ferner,astrid). %no tuvieron hijos
+casados(frederick, mary_donaldson).
+casados(harald_v,sonia_haraldsen).
+casados(joachim,alexandra_manley). %no tuvieron hijos
+casados(joachim,marie_cavallier).
+casados(jefferson_pfeil,alexandra). %no tuvieron hijos
+casados(alexander_johannsmaanny,nathalie).
+casados(paulos,maria_chantal).
+casados(ari_behn,martha_louise).
+casados(haakon_magnus,mette_marit_tjessem).
+
+
+% Hijos - Padres
+
+padres(louise_de_hesse_cassel,dagmar).
+padres(louise_de_hesse_cassel,george_I).
+padres(louise_de_hesse_cassel,frederik_vIII).
+padres(louise_de_hesse_cassel,thyra).
+padres(louise_de_hesse_cassel,valdemar).
+padres(louise_de_hesse_cassel,alexandra).
+padres(louise_de_suecia,cristian_x).
+padres(louise_de_suecia,louise).
+padres(louise_de_suecia,harald).
+padres(louise_de_suecia,ingeborg).
+padres(louise_de_suecia,gustavus).
+padres(alexandra,maud).
+padres(alexandrine_mecklenburg,frederik_lx).
+padres(alexandrine_mecklenburg,knud).
+padres(helen_holstein,carolina_matilda).
+padres(ingeborg,margaret).
+padres(ingeborg,astrid).
+padres(ingeborg,carl).
+padres(ingri_de_suecia,margrete_II).
+padres(ingri_de_suecia,benedikte).
+padres(ingri_de_suecia,anne_marie).
+padres(carolina_matilda,elisabeth).
+padres(carolina_matilda,ingolf).
+padres(carolina_matilda,christian).
+padres(henrry_de_laborde_de_monpezat,frederick).
+padres(henrry_de_laborde_de_monpezat,joachim).
+padres(benedikte,gustav).
+padres(benedikte,alexandra).
+padres(benedikte,nathale).
+padres(anne_marie,alexia).
+padres(anne_marie,paulos).
+padres(sonia_haraldsen,martha_louise).
+padres(sonia_haraldsen,haakon_magnus).
+padres(mary_donaldson,christian).
+padres(mary_donaldson,isabella).
+padres(mary_donaldson,vincent).
+padres(mary_donaldson,josephine).
+padres(marie_cavallier,nicolas).
+padres(marie_cavallier,felix).
+padres(marie_cavallier,henrik).
+padres(marie_cavallier,atena).
+padres(nathalie,contantino).
+padres(maria_chantal,maria_olympia).
+padres(maria_chantal,constantino).
+padres(maria_chantal,achilea).
+padres(maria_chantal,adyssea).
+padres(maria_chantal,aristides).
+padres(martha_louise,maud).
+padres(martha_louise,leah).
+padres(martha_louise,emma).
+padres(mette_marit_tjessem,ingrid).
+padres(mette_marit_tjessem,sverre).
+padres(mette_marit_tjessem,marius).
+
+
